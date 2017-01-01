35d8d5a923fe309adb32e5c30432d41225c5bc61
arunarjunakani <arunarjunakani@gmail.com>
2016-12-31 18:17:55 -0600

    Initial Commit

---
+++ src/RiddleSolver.java
@@ -0,0 +1,34 @@
+/**
+ * Created by arun on 12/31/16.
+ */
+public class RiddleSolver {
+
+    /**
+     * This is the main method where all of the solving will be happening
+     * @param String[]
+     * @return void
+     */
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * This method will read the rules and see if any definitive options can removed
+     * @param null
+     * @return void
+     */
+    public static void readRules()
+    {
+
+    }
+
+    /**
+     * This method will read the current data array and see if any cells can be definitively proven
+     * @param null
+     * @return void
+     */
+    public static void eliminateOptions()
+    {
+
+    }
+}
