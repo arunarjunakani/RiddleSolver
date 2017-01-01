caa9820a7c715cccb4b5a3f4f0cec560b6c5e685
arunarjunakani <arunarjunakani@gmail.com>
2016-12-31 19:27:17 -0600

    Created Data Array

--- src/RiddleSolver.java
+++ src/RiddleSolver.java
@@ -1,7 +1,12 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Created by arun on 12/31/16.
  */
 public class RiddleSolver {
+
+    public static String[][][] data;
 
     /**
      * This is the main method where all of the solving will be happening
@@ -9,7 +14,45 @@ public class RiddleSolver {
      * @return void
      */
     public static void main(String[] args) {
+        data = new String[5][5][];
+        for(int i = 0; i < 5; i++)
+        {
+            for(int j = 0; j < 5; j++)
+            {
+                switch(i)
+                {
+                    case 0:
+                        data[i][j] = new String[]{"B", "G", "R", "W", "Y"};
+                        break;
+                    case 1:
+                        data[i][j] = new String[]{"B", "D", "G", "N", "S"};
+                        break;
+                    case 2:
+                        data[i][j] = new String[]{"Ble", "Blu", "D", "Pa", "Pr"};
+                        break;
+                    case 3:
+                        data[i][j] = new String[]{"B", "C", "M", "T", "W"};
+                        break;
+                    case 4:
+                        data[i][j] = new String[]{"B", "C", "D", "F", "H"};
+                        break;
+                }
+            }
+        }
 
+        displayData();
+    }
+
+    private static void displayData()
+    {
+        for (String[][] row: data)
+        {
+            for(String[] cell: row)
+            {
+                System.out.print(Arrays.toString(cell));
+            }
+            System.out.println();
+        }
     }
 
     /**
@@ -17,7 +60,7 @@ public class RiddleSolver {
      * @param null
      * @return void
      */
-    public static void readRules()
+    private static void readRules()
     {
 
     }
@@ -27,8 +70,25 @@ public class RiddleSolver {
      * @param null
      * @return void
      */
-    public static void eliminateOptions()
+    private static void eliminateOptions()
     {
 
+    }
+
+    /**
+     * This method will check to see if the fish has been found
+     * @param null
+     * @return boolean
+     */
+    private boolean gameWon()
+    {
+        for(String[] cell: data[4])
+        {
+            if(cell.length == 1 && cell[0].equals("Fish"))
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
